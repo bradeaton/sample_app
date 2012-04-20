75846eae16171c09e6c260d28f5707bf15575005
bradeaton <brad@eaton-family.com>
2012-04-20 13:15:29 -0500

    Chapter 7 Exercise Changes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,18 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "error messages" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('Password can\'t be blank') }
+        it { should have_content('Name can\'t be blank') }
+        it { should have_content('Email is invalid') }
+        it { should have_content('Password is too short') }
+        it { should have_content('Password confirmation can\'t be blank') }
+      end
+      
     end
 
     describe "with valid information" do
@@ -42,8 +54,19 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+      
     end
   end
+  
+  
   
   
 end
